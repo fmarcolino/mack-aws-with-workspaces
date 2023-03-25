f26d326ce4473a6f31dd6010d887947d56836860
Francisco Marcolino Rodrigues Filho <fmarcolino@live.com>
2023-03-25 19:51:02 -0300

    chore: adopt resouce pattern name

--- resources/aws-key-pair.tf
+++ resources/aws-key-pair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "deployer" {
-  key_name   = "mackenzie"
+  key_name   = "keypair-mackenzie-${terraform.workspace}"
   public_key = trimspace(tls_private_key.this.public_key_openssh)
 
   tags = local.tags
--- resources/aws-mysql.tf
+++ resources/aws-mysql.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "main" {
-  name       = "mydb"
+  name       = "dbsubnetgroup-mysql-${terraform.workspace}"
   subnet_ids = values(aws_subnet.private).*.id
 
   tags = merge({
