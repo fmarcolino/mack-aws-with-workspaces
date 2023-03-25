b88a2702dfcddde10642ec9e4614702fb7dd1373
Francisco Marcolino Rodrigues Filho <fmarcolino@live.com>
2023-03-25 19:16:20 -0300

    feat: activate ping command

--- resources/aws-sgs-rules.tf
+++ resources/aws-sgs-rules.tf
@@ -23,6 +23,14 @@ resource "aws_security_group_rule" "instances-443-all" {
   security_group_id = aws_security_group.main["web_instances"].id
 }
 
+resource "aws_security_group_rule" "instances-icmp-all" {
+  type              = "ingress"
+  from_port         = -1
+  to_port           = -1
+  protocol          = "icmp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.main["web_instances"].id
+}
 
 resource "aws_security_group_rule" "database_by_instances" {
   type              = "ingress"
